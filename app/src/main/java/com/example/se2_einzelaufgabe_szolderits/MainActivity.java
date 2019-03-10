da6134c30933c0102c6e935ce6db2fdea1434649
Szolderits Chiara <c.szolderits@gmail.com>
2019-03-10 18:25:03 +0100

    if-else for setting text, if no student number is typed

--- app/src/main/java/com/example/se2_einzelaufgabe_szolderits/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelaufgabe_szolderits/MainActivity.java
@@ -21,17 +21,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public void calculate(View view) {
+
         String studentNumber = inp.getText().toString();
 
+        if (studentNumber.isEmpty()) {
+            txtOut.setText("Bitte Matrikelnummer eingeben!");
+        } else {
             int[] zahlArray = new int[studentNumber.length()];
 
             for (int i = 0; i < studentNumber.length(); i++) {
                 zahlArray[i] = Integer.parseInt(studentNumber.substring(i, i + 1));
             }
 
-            int product=1;
+            int product = 1;
             int j = 0;
 
             while (j < zahlArray.length) {
@@ -47,8 +50,7 @@ public class MainActivity extends AppCompatActivity {
             String solution = Integer.toString(product);
             txtOut.setText(solution);
         }
-
-
+    }
 
 
 }
